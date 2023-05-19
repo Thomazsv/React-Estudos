d2fe3b41ba325f396d1bd99d940e038a15ec6753
Thomazsv <felipethomazsv@gmail.com>
2023-05-19 17:36:57 -0300

    iniciando react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ItemGastos from "./components/ItemGastos";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ola mundo!</h1>
+      <p>rfce é muito brabo!</p>
+      <ItemGastos></ItemGastos>
     </div>
   );
 }
---
+++ src/components/ItemGastos.js
@@ -0,0 +1,12 @@
+import "./ItemGastos.css";
+function ItemGastos() {
+  const titulo = "Item de Gastos";
+  return (
+    <h2 className="gastos" style={{ color: "green" }}>
+      {titulo}
+    </h2>
+  );
+}
+
+export default ItemGastos;
+// style={color:red}
