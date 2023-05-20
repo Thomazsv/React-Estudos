77b8e46547f7e213a2689f335dd6e16d31547851
Thomazsv <felipethomazsv@gmail.com>
2023-05-20 17:01:29 -0300

    continuando

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import ItemGastos from "./components/ItemGastos";
 
 function App() {
+  const dados = [{ titulo: "Item de Gastos", preco: 50, data: "10/01/22" }];
   return (
     <div>
       <h1>Ola mundo!</h1>
       <p>rfce é muito brabo!</p>
-      <ItemGastos></ItemGastos>
+      <ItemGastos
+        titulo={dados[0].titulo}
+        preco={dados[0].preco}
+        data={dados[0].data}
+      />
+      <ItemGastos
+        titulo={dados[0].titulo}
+        preco={dados[0].preco}
+        data={dados[0].data}
+      />
     </div>
   );
 }
--- src/components/ItemGastos.js
+++ src/components/ItemGastos.js
@@ -1,10 +1,14 @@
 import "./ItemGastos.css";
-function ItemGastos() {
-  const titulo = "Item de Gastos";
+function ItemGastos(props) {
+  // const titulo = "Item de Gastos";
   return (
-    <h2 className="gastos" style={{ color: "green" }}>
-      {titulo}
-    </h2>
+    <div className="card">
+      <h2 className="gastos" style={{ color: "green" }}>
+        {props.titulo}
+      </h2>
+      <p>{props.preco}</p>
+      <p>{props.data}</p>
+    </div>
   );
 }
 
